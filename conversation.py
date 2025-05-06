7971065459c66de9e24d1c5d491713e88a46b5a3
ManI'mDead <cte-elijah.snyder@sllboces.org>
2025-05-06 17:44:37 +0000

    Tuesday Python Assignment

---
+++ conversation.py
@@ -0,0 +1,8 @@
+name = input('What is your first name? ')
+print('Hello ' + name + '!')
+
+feeling = input('How are you feeling today? ')
+print('Oh you feel ' + feeling + ', ok!')
+
+town = input('What town do you live in? ')
+print('So your name is ' + name + ', you feel ' + feeling + ', and you live in ' + town + '!')
